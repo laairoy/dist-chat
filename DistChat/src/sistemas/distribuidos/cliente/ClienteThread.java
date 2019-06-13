a3c931be27b3574cee0dcf2be3e06d4e547f918b
Marden Laairoy <laairoy@yahoo.com.br>
2019-06-13 00:34:47 -0300

    Corrigi bug que faz lista de clientes sumir.

--- DistChat/src/sistemas/distribuidos/cliente/ClienteThread.java
+++ DistChat/src/sistemas/distribuidos/cliente/ClienteThread.java
@@ -8,6 +8,10 @@ package sistemas.distribuidos.cliente;
 import sistemas.distribuidos.distchat.ClienteListModel;
 import java.io.InputStream;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.SwingUtilities;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import sistemas.distribuidos.distchat.JsonConvert;
@@ -17,28 +21,28 @@ import sistemas.distribuidos.distchat.JsonConvert;
  * @author laairoy
  */
 public class ClienteThread extends Thread {
-    
+
     private final InputStream entradaDados;
-    
+
     public ClienteThread(InputStream entradaDados) {
         this.entradaDados = entradaDados;
     }
-    
+
     @Override
     public void run() {
         Scanner recebido = new Scanner(entradaDados);
         while (recebido.hasNextLine()) {
-            
+
             String msg = recebido.nextLine();
             System.out.println("[RECEBIDO] <- " + msg);
             verificarOperacao(msg);
-            
+
         }
     }
-    
+
     private void verificarOperacao(String msg) {
         JsonConvert json = new JsonConvert(msg);
-        
+
         if (json.getCod().equals("lista")) {
             getList(json.getList());
         }
@@ -48,16 +52,23 @@ public class ClienteThread extends Thread {
             }
         }
     }
-    
+
     private void getList(JSONArray lista) {
-        ClienteListModel cliList = ClienteListModel.init();        
+        ClienteListModel cliList = ClienteListModel.init();
         cliList.removeAll();
-        
+
         for (Object json : lista) {
             JSONObject temp = new JSONObject(json.toString());
             cliList.addElement(temp.getString("NOME"), temp.getString("IP"), Integer.parseInt(temp.getString("PORTA")));
+            try {
+                TimeUnit.MILLISECONDS.sleep(10);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(ClienteThread.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
+
         
+
     }
-    
+
 }
